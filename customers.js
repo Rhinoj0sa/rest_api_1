09742407030fcb974da08997c4ff3da6cb5c51a4
Rhinoj0sa <rhinojosaram@gmail.com>
2023-08-22 09:55:05 -0600

    added customer and endpoinyt

---
+++ customers.js
@@ -0,0 +1,52 @@
+const customer_list = [
+    {
+        "name": "johnny Canales",
+        "email": "john@abc.com",
+        "phone": "1234567890",
+        "suscribed": ["Sports", "Finance"],
+        "channels": ["SMS"]
+    },
+    {
+        "name": "Miguel Gonzalez",
+        "email": "miguel@abc.com",
+        "phone": "1234567890",
+        "suscribed": ["Sports", "Finance"],
+        "channels": ["SMS", "Email"]
+    },
+    {
+        "name": "Arturo Perez",
+        "email": "arturog@abc.com",
+        "phone": "1234567890",
+        "suscribed": ["Sports", "Finance"],
+        "channels": ["SMS", "Email"]
+    },
+    {
+        "name": "Ricardo Perez",
+        "email": "ricardo@abc.com",
+        "phone": "1234567890",
+        "suscribed": ["Sports", "Finance", "Films"],
+        "channels": ["SMS", "Email"]
+    },
+    {
+        "name": "Julio Martinez",
+        "email": "julio@abc.com",
+        "phone": "1234567890",
+        "suscribed": ["Sports", "Finance", "Films"],
+        "channels": ["SMS", "Email"]
+    },
+    {
+        "name": "Brian Wilson",
+        "email": "brian@abc.com",
+        "phone": "1234567890",
+        "suscribed": ["Sports", "Finance", "Films"],
+        "channels": ["SMS", "Email"]
+    },
+    {
+        "name": "John Smith",
+        "email": "johnny@abc.com",
+        "phone": "1234567890",
+        "suscribed": ["Sports", "Finance", "Films"],
+        "channels": ["SMS", "Email"]
+    }
+
+]
