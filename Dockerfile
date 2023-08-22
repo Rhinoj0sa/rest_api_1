8f03330e358d2030809ad6b546dea9b3d2541b98
Rhinoj0sa <rhinojosaram@gmail.com>
2023-08-21 23:27:35 -0600

    added docker files

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:alpine
+WORKDIR /usr/src/app
+COPY package.json .
+COPY package-lock.json .
+RUN npm ci
+COPY . .
+# CMD ["npm", "start"]
+CMD ["npm","run", "dev"]
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const routes = require('./routes/index.js');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/restapis', {
+mongoose.connect('mongodb://mongo_db:27017', {
     useNewUrlParser: true,
 
 });
@@ -14,6 +14,6 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes()); 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 // app.get('/', (req, res) => res.send('Hello World!'));
-// app.get('/test', (req, res) => res.send('Test!'));
+app.get('/test', (req, res) => res.send('Test!'));
