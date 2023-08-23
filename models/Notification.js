2d4761493577fa5278deadc01f8a2441fc56f79a
Roberto Hinojosa <rhinojosaram@gmail.com>
2023-08-22 20:45:54 -0600

    Notifications (#3)
    * modified the messageController to create the list of modifications
    
    * modified the messageController.js works fine

---
+++ models/Notification.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const notificationSchema = new Schema({
+    name: {
+        type: String,
+        trim: true
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        trim: true
+    },
+    phone: {
+        type: String,
+        trim: true
+    },
+    text: {
+        type: String,
+        trim: true
+    },
+    category: {
+        type: String,
+        trim: true
+    },
+    channel:{
+        type: String,
+        trim: true
+    },
+    sent: {
+        type: Boolean,
+        default: false
+    }
+},{timestamps:true});
+module.exports = mongoose.model('Notification', notificationSchema);
