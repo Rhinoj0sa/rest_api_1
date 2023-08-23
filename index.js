5f1488f1f9d7dbf80163b8863b3276a5e332b9a0
Rhinoj0sa <rhinojosaram@gmail.com>
2023-08-23 14:32:28 -0600

    Added .env 4 config

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require('express');
 const routes = require('./routes/index.js');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://mongo_db:27017', {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-
 });
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 // send the control to routes/index.js
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes()); 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${process.env.PORT}!`));
 // app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/test', (req, res) => res.send('Test!'));
