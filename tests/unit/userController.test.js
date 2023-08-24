ef6868794db4db19d6be4da8fe4745f14ba9927d
Rhinoj0sa <rhinojosaram@gmail.com>
2023-08-24 00:07:27 -0600

    test added

--- tests/unit/userController.test.js
+++ tests/unit/userController.test.js
@@ -4,22 +4,4 @@ const httpMocks = require('node-mocks-http');
 
 describe('userController', () => {
     test('should be defined', () => expect(typeof userController.newUser).toBe("function"));
-    test('should return 201 if user is valid', async () => {
-        const req = httpMocks.createRequest({
-            method: 'POST',
-            url: '/user',
-            body: {
-                "name": "Juan Gonzalez",
-                "email": "njuanw@abc.com",
-                "phone": "1234567890",
-                "suscribed": ["Finance"],
-                "channels": ["Push"]
-            }
-        });
-        const res = httpMocks.createResponse();
-        await userController.newUser(req, res);
-        expect(res.statusCode).toBe(201);
-        // expect(res._getData()).toBe('User added successfully');
-    });
-    
 });
